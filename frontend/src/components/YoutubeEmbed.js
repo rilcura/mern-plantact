16986d51cf258931867371a2fc74e74e26769c2d
Randzel Ivan L. Cura <randzellcura@gmail.com>
2022-11-29 14:43:45 +0200

    Fixed responsiveness of youtube videos on preventionscreen

--- frontend/src/components/YoutubeEmbed.js
+++ frontend/src/components/YoutubeEmbed.js
@@ -4,8 +4,7 @@ import PropTypes from "prop-types";
 const YoutubeEmbed = ({ embedId }) => (
     <div className="video-responsive">
         <iframe
-            width="853"
-            height="480"
+            className="min-vh-100 w-100"
             src={`https://www.youtube.com/embed/${embedId}`}
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
